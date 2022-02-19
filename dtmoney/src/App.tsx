8ba79c1e5407b9a0735a3e57a6ac8d6d9c8b19a8
ArmandoMarioto <armando_marioto100@hotmail.com>
2022-02-19 15:54:16 -0300

    Criando estilos globais

--- dtmoney/src/App.tsx
+++ dtmoney/src/App.tsx
@@ -1,14 +1,13 @@
-import styled from "styled-components";
 
-const TitleComponent = styled.h1`
-  font-size: 60px;
-  color: #8257e6
-`;
+import { GlobalStyle } from "./styles/global";
+
+
 
 export function App() {
   return (
     <div className="App">
-      <TitleComponent>Hello World</TitleComponent>
+      <h1>Hello World</h1>
+      <GlobalStyle />
     </div>
   );
 }
