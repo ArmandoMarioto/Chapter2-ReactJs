e872b56c221eae5dcef5e04b72d0e66aa09346dd
ArmandoMarioto <armando_marioto100@hotmail.com>
2022-02-20 14:54:09 -0300

    Configurando MirageJs

--- dtmoney/src/index.tsx
+++ dtmoney/src/index.tsx
@@ -1,6 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createServer } from 'miragejs';
 import { App } from './App';
+
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Compras de Janeiro',
+          amount: 1000,
+          type: 'outcome',
+          category: 'Roupas',
+          createAt: new Date(),
+        },
+      ];
+     })
+  }
+});
 
 ReactDOM.render(
   <React.StrictMode>
